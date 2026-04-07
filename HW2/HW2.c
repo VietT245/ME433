36c2616e3e88f9d52bbc956a50903954a92ac90c
VietT245 <60241601+VietT245@users.noreply.github.com>
2026-04-07 00:21:02 -0500

    Added slice and channel plus pwm conditions

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -5,12 +5,21 @@
 int main()
 {
     const uint pin = 16;
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+
+    uint slice = pwm_gpio_to_slice_num(pin);
+    uint channel = pwm_gpio_to_channel(pin);
+
+    pwm_set_wrap(slice, 20000); //20 µs period (50hz)
+    pwm_set_enabled(slice, true);
+    
     while (1) {
         //Sweep 1000–2000 µs
         for (int us = 1000; us <= 2000; us++){
             pwm_set_chan_level(slice, channel, us);
             sleep_ms(10);
         }
+        // Sweep backward
         for (int us = 2000; us >= 1000; us--){
             pwm_set_chan_level(slice, channel, us);
             sleep_ms(10);
