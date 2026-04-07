04df72100a8810889bb99906c4397cf67cd36bf7
VietT245 <60241601+VietT245@users.noreply.github.com>
2026-04-07 00:05:58 -0500

    Created angle_to_µs function

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -3,6 +3,10 @@
 #include "hardware_pwm.h"
 #include <unistd.h>
 
+// Converting angle to pulse width (in µs)
+static inline int angle_to_us(int angle){
+    return 1000 + (angle * 1000) / 180; // 1000 - 2000 µs
+}
 
 int main()
 {
