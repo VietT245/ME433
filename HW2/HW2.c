7f6582165027b95ee06405b5a24a09513ba009cb
VietT245 <60241601+VietT245@users.noreply.github.com>
2026-04-07 01:03:24 -0500

    adjusted clkdiv and wrap

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -8,18 +8,21 @@ int main()
     sleep_ms(2000);
 
     const uint pin = 0;
+    const float clkdiv = 125.0f;
+    const uint16_t wrap = 20000;
     gpio_set_function(pin, GPIO_FUNC_PWM);
 
     uint slice = pwm_gpio_to_slice_num(pin);
     uint channel = pwm_gpio_to_channel(pin);
-    pwm_set_clkdiv(slice, 2.0f);
 
-    pwm_set_wrap(slice, 20000); //20 ms period (50hz)
+    pwm_set_clkdiv(slice, clkdiv);
+    pwm_set_wrap(slice, wrap);
+
     pwm_set_enabled(slice, true);
 
     while (1) {
         //Sweep 1000–2000 µs
-        for (int us = 1000; us <= 2000; us++){
+        for (int us = 1000; us <= 2000; us+= 10){
             pwm_set_chan_level(slice, channel, us);
 
             // Convert pulse width to angle (approx)
@@ -30,7 +33,7 @@ int main()
             sleep_ms(10);
         }
         // Sweep backward
-        for (int us = 2000; us >= 1000; us--){
+        for (int us = 2000; us >= 1000; us-= 10){
             pwm_set_chan_level(slice, channel, us);
 
             float angle = (us - 1000) * (180.0f / 1000.0f);
