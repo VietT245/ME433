7366820c87a3d78e7f6a50a3f0c96958ceb7c750
VietT245 <60241601+VietT245@users.noreply.github.com>
2026-04-07 00:19:19 -0500

    set pin and for loops for Sweeps

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -2,11 +2,18 @@
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
 
-
-int main(void)
+int main()
 {
-    
+    const uint pin = 16;
     while (1) {
-
+        //Sweep 1000–2000 µs
+        for (int us = 1000; us <= 2000; us++){
+            pwm_set_chan_level(slice, channel, us);
+            sleep_ms(10);
+        }
+        for (int us = 2000; us >= 1000; us--){
+            pwm_set_chan_level(slice, channel, us);
+            sleep_ms(10);
+        }
     }
 }
