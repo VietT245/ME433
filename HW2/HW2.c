5f27d47205d6c50fe6bacd36d42fe42fa329e83c
VietT245 <60241601+VietT245@users.noreply.github.com>
2026-04-07 00:06:49 -0500

    Update HW2.c

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -8,11 +8,12 @@ static inline int angle_to_us(int angle){
     return 1000 + (angle * 1000) / 180; // 1000 - 2000 µs
 }
 
-int main()
+int main(void)
 {
-    stdio_init_all();
+    pwm_t servo = pwm_open(0); // PWM channel 0
+    pwm_set_freq(servo, 50); //50 Hz for RC servo
 
-    while (true) {
+    while (1) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
