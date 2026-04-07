1fd715a8634e6fb391bff58d2aaeee4ed8540c83
VietT245 <60241601+VietT245@users.noreply.github.com>
2026-04-07 00:14:57 -0500

    Deleted incorrect code

--- HW2/HW2.c
+++ HW2/HW2.c
@@ -1,29 +1,12 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware_pwm.h"
-#include <unistd.h>
+#include "hardware/pwm.h"
 
-// Converting angle to pulse width (in µs)
-static inline int angle_to_us(int angle){
-    return 1000 + (angle * 1000) / 180; // 1000 - 2000 µs
-}
 
 int main(void)
 {
-    pwm_t servo = pwm_open(0); // PWM channel 0
-    pwm_set_freq(servo, 50); //50 Hz for RC servo
-
+    
     while (1) {
-        // Sweep 0 -> 180
-        for (int a = 0; a <= 180; a++){
-            pwm_set_duty_us(servo, angle_to_us(a));
-            usleep(10000); //10 µs step
-        }
 
-        // Sweep 180 -> 0
-        for (int a = 180; a >= 0; a--){
-            pwm_set_duty_us(servo, angle_to_us(a));
-            usleep(10000);
-        }
     }
 }
