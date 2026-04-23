1cb14ccf29e0fa6d353d9f5d05fba23b0514ffaf
VietT245 <60241601+VietT245@users.noreply.github.com>
2026-04-23 12:59:19 -0500

    Defined pins

--- HW4/HW4.c
+++ HW4/HW4.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
 
+// I2C settings
+#define I2C_PORT i2c0
+#define I2C_SDA 4
+#define I2C_SCL 5
+#define MCP23008_ADDR 0x20
+
+// MCP23008 registers
+#define IODIR 0x00
+#define GPIO 0x09
+#define OLAT 0x0A
+
+// Pico heartbeat LED
+#define HEARTBEAT_LED 7
+#define BUTTON_PIN 0
 
 int main()
 {
